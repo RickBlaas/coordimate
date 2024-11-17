4f5ebff1691d08d23bfdb9fbddd93daca5242a75
Rick <rickblaas@live.nl>
2024-11-17 13:00:18 +0100

    Add initial Flutter project structure with platform-specific configurations and assets. also made the register/login interact with the school api

---
+++ lib/pages/login.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_login/flutter_login.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:logger/logger.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final storage = const FlutterSecureStorage();
+  final logger = Logger();
+
+  Duration get loginTime => const Duration(milliseconds: 2250);
+
+  Future<String?> _authUser(LoginData data) async {
+    final response = await http.post(
+      Uri.parse('https://team-management-api.dops.tech/api/v2/auth/login'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'name': data.name,
+        'password': data.password,
+      }),
+    );
+
+    logger.d('Response status: ${response.statusCode}');
+    logger.d('Response body: ${response.body}');
+    if (response.statusCode == 200) {
+      final responseJson = jsonDecode(response.body);
+      final jwt = responseJson['token'];
+      // Store the JWT securely
+      await storage.write(key: 'jwt', value: jwt);
+
+      // Use a mounted check before navigating
+      if (mounted) {
+        Navigator.of(context).pushReplacementNamed('/home');
+      }
+      return null; // Login successful
+    } else {
+      return 'Username or password is incorrect';
+    }
+  }
+
+  Future<String?> _registerUser(SignupData data) async {
+    final response = await http.post(
+      Uri.parse('https://team-management-api.dops.tech/api/v2/auth/register'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, String>{
+        'name': data.name!,
+        'password': data.password!,
+      }),
+    );
+
+    // Log the response for debugging
+    logger.d('Response status: ${response.statusCode}');
+    logger.d('Response body: ${response.body}');
+    final responseJson = jsonDecode(response.body);
+
+    if (response.statusCode == 201) {
+      return null; // Registration successful
+    } else {
+      final errorMessage = responseJson['message'] ?? 'Registration failed';
+      logger.e('Registration failed: $errorMessage');
+      return errorMessage;
+    }
+  }
+
+  Future<String?> _recoverPassword(String name) async {
+    // Dummy implementation
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlutterLogin(
+      title: 'Coordimate',
+      onLogin: (loginData) => _authUser(loginData),
+      onSignup: _registerUser,
+      onRecoverPassword: _recoverPassword,
+      onSubmitAnimationCompleted: () {
+        // Navigate only if the widget is still mounted
+        if (mounted) {
+          Navigator.of(context).pushReplacementNamed('/home');
+        }
+      },
+      hideForgotPasswordButton: true,
+      messages: LoginMessages(
+        userHint: 'Username',
+        passwordHint: 'Password',
+        confirmPasswordHint: 'Confirm Password',
+        loginButton: 'LOGIN',
+        signupButton: 'REGISTER',
+        goBackButton: 'BACK',
+        confirmPasswordError: 'Passwords do not match!',
+      ),
+      userValidator: (value) {
+        if (value == null || value.isEmpty) {
+          return 'Username is required';
+        }
+        return null;
+      },
+    );
+  }
+}
